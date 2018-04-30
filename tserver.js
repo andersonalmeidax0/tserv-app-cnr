3954f4275c7b5fe5d94b0acfc547ef64a6c130c8
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 22:43:27 -0300

    Create tserver.js

---
+++ tserver.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+var currentdate = new Date(); 
+var datetime = 
+                 currentdate.getFullYear() + "-"  + (currentdate.getMonth()+1)  + "-"  +  currentdate.getDate() + "-"
+                + currentdate.getHours() + "-" + currentdate.getMinutes() + "-"  + currentdate.getSeconds();
+
+  res.end('{"Hello Node.JS!","'+datetime+'"}');
+}).listen(9090);
+
+console.log('Server running at http://localhost:9090/');
+
