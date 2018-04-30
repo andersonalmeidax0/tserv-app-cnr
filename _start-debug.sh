c01022b332267f4d45ae4e7d922951fe1f812b06
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 22:49:34 -0300

    Update _start-debug.sh

--- _start-debug.sh
+++ _start-debug.sh
@@ -1,3 +1,4 @@
+#DEBUG: tirar -d e colocar -t -i com /bin/bash no final
 #parameter -d =>   detached mode, ou seja como deamon
 #parameter -p HOSTPORT:CONTAINER_PORT
-sudo docker run -d --name tserv-app-cnr -p 8090:9090  tserv-app-img
+sudo docker run -t -i --name tserv-app-cnr -p 8090:9090  tserv-app-img /bin/bash
