95a69a384620a959be4d9b7805d2c76507782022
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-05-06 23:38:29 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -4,4 +4,4 @@
 #sudo docker run -p 8080:8080 -v $PWD:/home -d --net=netdb  --name nodedb node-db-server
 
 
-sudo docker run -d --name tserv-app-cnr -p 9999:9999 -v /var/apps/tserv:/var  tserv-app-img
+sudo docker run -d -p 9999:9999 -v /var/apps/tserv:/var --name tserv-app-cnr  tserv-app-img
