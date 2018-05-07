fd221e7730e368db4925c6ce57ab6c276ee8145d
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-05-06 22:44:19 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -1,3 +1,7 @@
 #parameter -d =>   detached mode, ou seja como deamon
 #parameter -p HOSTPORT:CONTAINER_PORT
-sudo docker run -d --name tserv-app-cnr -p 8090:9090  tserv-app-img
+#sudo docker run -p 5984:5984 -d -v /opt/couchdb/data:/opt/couchdb/data --net=netdb --name mycouchdb couchdb 
+#sudo docker run -p 8080:8080 -v $PWD:/home -d --net=netdb  --name nodedb node-db-server
+
+
+sudo docker run -d --name tserv-app-cnr -p 9999:9999 -v /var/apps/tserv:/var  tserv-app-img
