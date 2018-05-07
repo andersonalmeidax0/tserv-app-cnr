cb920de3c17237bbc8446d131de1ecbf61f55fe6
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-05-07 01:07:54 -0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,4 +1,5 @@
 #o ultimo parametro eh o contexto de COPY, etc
 #sudo docker build --no-cache -t="tserv-app-img" -f ./app.Dockerfile .
 sudo docker build  -t="tserv-app-img" -f ./app.Dockerfile .
-sudo docker push tserv-app-img
+#para ser mais rapido, tire o push
+#sudo docker push tserv-app-img
