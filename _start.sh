faaac5089ba2193d4dd79e1ddafd21faa49af348
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-07-01 22:51:32 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -2,4 +2,4 @@
 #parameter -p HOSTPORT:CONTAINER_PORT
 #sudo docker run -p 5984:5984 -d -v /opt/couchdb/data:/opt/couchdb/data --net=netdb --name mycouchdb couchdb 
 #sudo docker run -p 8080:8080 -v $PWD:/home -d --net=netdb  --name nodedb node-db-server
-sudo docker run -d -p 9999:9999 -v ../data/tserv:/data --name tserv-app-cnr  tserv-app-img
+sudo docker run -d -p 9999:9999 -v /home/anderson/data/tserv:/data --name tserv-app-cnr  tserv-app-img
