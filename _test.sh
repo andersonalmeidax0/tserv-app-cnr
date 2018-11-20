e4bf99fcff8b2eb5c85c5992bd987896ece893f5
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-11-20 01:13:27 -0200

    Update _test.sh

--- _test.sh
+++ _test.sh
@@ -1,2 +1,6 @@
+sudo docker logs tserv-app-cnr
+echo "-----------------"
 response=$(curl -X GET localhost:9999/health);
-if [ "${response}" == "APPSERV OK" ]; then echo "SUCESS"; fi;
+echo "-----------------"
+if [ "${response}" == "APPSERV OK" ]; then echo "SUCESS!!!!!!!"; fi;
+echo "-----------------"
