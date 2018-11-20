b42c30b9e5a360dfe027a85485abe1d904418e6a
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-11-19 22:19:54 -0200

    Update tserver.js

--- tserver.js
+++ tserver.js
@@ -1,15 +1,32 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-var currentdate = new Date(); 
-var datetime = 
+http.createServer(function (req, res) 
+{
+  if(request.method === "GET" && request.url==="/health")
+  {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('{"Status":"OK"}');
+  }
+  else
+  if(request.method === "GET" && request.url==="/time")
+  {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var currentdate = new Date(); 
+    var datetime = 
                  currentdate.getFullYear() + "-"  + (currentdate.getMonth()+1)  + "-"  +  currentdate.getDate() + "-"
                 + currentdate.getHours() + "-" + currentdate.getMinutes() + "-"  + currentdate.getSeconds();
 
-  res.end('{"Hello Node.JS!","'+datetime+'"}');
-  console.log(datetime);
-}).listen(9999);
+     res.end('{"Hello Node.JS!","'+datetime+'"}');
+     console.log(datetime);
+  }
+ else
+ {
+    response.writeHead(404, 'Resource Not Found', {'Content-Type': 'text/plain'});
+    response.end('{"Status":"not found"}');
+ }
+   
+}
+).listen(9999);
 
 console.log('Server running at http://localhost:9999/');
 
